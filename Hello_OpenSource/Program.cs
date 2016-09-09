cdc0bf17b8ffb1bb18ba169d8e84e38d2c3262de
wittawatz7 <wittawat.harn57@sskru.ac.th>
2016-09-09 14:26:00 +0700

    add project to repository

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+namespace Hello_OpenSource
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello My Name is wittawat harnchai 5711423139");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
